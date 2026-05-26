b8847475f3f88f46a1cb7d1a4e21e09b98791140
Luka <metaluca27@users.noreply.github.com>
2026-05-26 20:35:59 +0900

    NABE Auto-Publish: 2026-05-26 20:35:59

--- config.py
+++ config.py
@@ -6,5 +6,6 @@ CONFIG = {
     "blog_title": "InfoLoomia",
     "blog_subtitle": "지식을 엮어 미래를 보다 - IT, 테크, AI 및 생산성 지식 정보",
     "author": "Luka",
-    "adsense_client": "",  # 애드센스 승인 완료 후 ca-pub-XXXXXXXXXXXXXXXX 번호를 입력하세요
+    "adsense_client": "ca-pub-3073744952767165",  # 애드센스 승인 완료 후 ca-pub-XXXXXXXXXXXXXXXX 번호를 입력하세요
 }
+
